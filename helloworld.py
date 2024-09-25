5200cf5ae8610d0862776c9f339134f589322de6
Aleksandr728418 <100728418@unimail.derby.ac.uk>
2024-09-25 12:58:10 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,17 @@
+print("helloworld")
+#that was first
+print(type(12+5))
+print(type(5+3.0))
+print(type(12/5))
+print(type(12+5==5*3.0))
+#that was third
+print("indentation matters in python")
+#4
+name = input("hello,what's your name?\n")
+age= input("what's your age?\n")
+print(f"hello {name}, your age is : {age}")
+#that was 5
 print("please enter a whole number:")
 x = int(input())
 print(f"{x}*10={x*10}")
+#6
