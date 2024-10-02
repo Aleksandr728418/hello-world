a8e1991b45a007e6df3a463d40f56bfce581bd3c
Aleksandr728418 <100728418@unimail.derby.ac.uk>
2024-10-02 11:30:40 +0100

    tasks2.1-2.3

---
+++ Task2/3.py
@@ -0,0 +1,10 @@
+print("Programming in python")
+print('Programming in Python with singleqoutes')
+print(f"I know how to put\"quotes\" into a string \n\nAnd put in new lines! ")
+print(f'''I am using multiline string to
+-print multiple lines
+-without the use of the escape character ''')
+str1 = "string 1"
+str2 = "string 2"
+print(f"Hi, I am {str1} and I am {str2}!")
+print(f"{str1} is before {str2} and {str2} is after {str1}")
---
+++ Task2/4.py
@@ -0,0 +1,3 @@
+a = float(input("please enter a number"))
+b = float(input("Please enter another number:"))
+print(f"The result of multiplying {a} by {b} is {a*b}")
